f8132e6e2a0bd80cd6e4722a65d7c6fc233ec88c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-03 14:19:14 +0200

    Adding data class, correcting naming convention plus syntax errors

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -1,12 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie_database_MVC.Data;
+using Movie_database_MVC.Models;
+
 
 namespace Movie_database_MVC.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly MovieDbContext _context;
+
+        public MovieController(MovieDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            _context.Entry(movie).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
