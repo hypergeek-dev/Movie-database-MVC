58ebdaf631c6890c060e796a2d1622a60bb1c672
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-03 11:11:56 +0200

    Added model, controller and view for searching movieDB

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_database_MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
