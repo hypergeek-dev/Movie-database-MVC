c03bf203340b433db9bb8b2bebf8ab602da7ac59
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-10-05 10:19:06 +0200

    Added partialviewcontroller and related partial views

---
+++ Controllers/PartialViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_database_MVC.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        public IActionResult MovieCreatePartial()
+        {
+            return PartialView("_MovieCreatePartial");
+        }
+    }
+}
